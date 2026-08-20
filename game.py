fcffb47295a06996887d3b640d2cf4e34547f161
drain-high-school <chosen@atomicmail.io>
2026-08-20 13:34:11 +0100

    Implement initial Yatzy game structure
    Added initial game setup for Yatzy with dice rolling logic.

--- game.py
+++ game.py
@@ -1,1 +1,16 @@
 
+# Yatzy
+
+turns_left = 13  # A turn consists of three rolls.
+current_hand = []
+
+def roll_dice(dice_to_reroll):
+    for roll in range(len(dice_to_reroll)):
+        if roll == True:
+            current_hand
+
+def roll(rolls_left, current_hand):
+    if rolls_left == 3:  # The user's turn just started, so roll the inital dice.
+        current_hand = roll_dice([True, True, True, True, True])
+        
+    user_input = input("Which dice would you like to reroll? Press Enter to reroll none.")
